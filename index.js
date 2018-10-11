4944a73480f75bc5a1d6d3ab1aacff7e8114c0a9
Joel Weiser <joel.weiser@oicr.on.ca>
2018-10-10 23:24:51 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var url = require('url');
+
+var server = http.createServer(function (req, res) {
+    var parsedUrl = url.parse(req.url, true);
+
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    var queryStringObject = parsedUrl.query;
+
+    var method = req.method.toLowerCase();
+    
+    res.end("Hello World\n");
+
+    console.log("Request received on path: " + trimmedPath + " with " + method + " method and query ", queryStringObject);
+});
+
+server.listen(3000, function() {
+    console.log("The server is listening on port 3000 now");
+});
