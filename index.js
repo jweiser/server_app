453d96eed370b6876c5bf5b98cb28902d4cb2b20
jweiser <joel.weiser@oicr.on.ca>
2018-10-18 10:02:52 -0400

    added routing

--- index.js
+++ index.js
@@ -10,8 +10,41 @@ var server = http.createServer(function (req, res) {
     var queryStringObject = parsedUrl.query;
 
     var method = req.method.toLowerCase();
+
+    var headers = req.headers;
     
-    res.end("Hello World\n");
+    var decoder = new StringDecoder('utf-8');
+    var buffer = '';
+    req.on('data', function(data){
+        buffer += decoder.write(data);
+    });
+    req.on('end', function() {
+        buffer += decoder.end();
+
+        var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+        var data = {
+            'trimmedPath' : trimmedPath,
+            'queryStringObject' : queryStringObject,
+            'method' : method,
+            'headers' : headers,
+            'payload' : buffer
+        };
+
+        chosenHandler(data, function(statusCode, payload) {
+            statusCode = typeof(statusCode) === 'number' ? statusCode : 200;
+            payload = typeof(payload) === 'object' ? payload : {};
+
+            var payloadString = JSON.stringify(payload);
+
+            res.writeHead(statusCode);
+            res.end(payloadString);
+
+            console.log('Returning this response: ', statusCode, payloadString);
+        });
+
+        res.end("Hello World\n");
+    });
 
     console.log("Request received on path: " + trimmedPath + " with " + method + " method and query ", queryStringObject);
 });
@@ -19,3 +52,17 @@ var server = http.createServer(function (req, res) {
 server.listen(3000, function() {
     console.log("The server is listening on port 3000 now");
 });
+
+var handlers = {};
+
+handlers.sample = function(data, callback) {
+    callback(406, {'name': 'sample handler'});
+};
+
+handlers.notFound = function(data, callback) {
+    callback(404);
+};
+
+var router = {
+    'sample' : handlers.sample
+};
